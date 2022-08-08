f5a455ac3a5fe92d07d44ee54d2fd50985c08cc0
Shreyas Manolkar <shreyasmanolkar123@gmail.com>
2022-08-08 19:09:19 +0530

    enabels cores

--- src/app.js
+++ src/app.js
@@ -21,6 +21,11 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '..' , 'public')));
 
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+});
+
 app.use('/v1', api);
 
 app.get('/*', (req, res)=>{
